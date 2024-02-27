a52ceb00a8d6b6be1e3f2c90d8ed587276fdf285
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-02-27 21:01:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const axios = require('axios');
-
-const app = express();
 const PORT = 3000;
 
+const app = express();
+const server = http.createServer(app);
+  app.set('view engine', 'ejs');
+  app.set('json spaces', 2);
+  app.use(express.static('views'));
+
 app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://web-app-bot-489f88bd8283.herokuapp.com/get');
+        const data = response.data.bot;
+        res.render('index', {
+           chatpc: data.chatpc
+           user: data.user
+           hit: data.hit
+           uptime: data.uptime
+      };
+    };
+  });
+
+app.get('/tes', async (req, res) => {
     try {
         // Lakukan permintaan ke api.xnd.me
         const response = await axios.get('https://web-app-bot-489f88bd8283.herokuapp.com/stats');
