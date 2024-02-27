0ff731f4f78346f8ba1ad07c3c7cff8cd646d22a
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-02-27 21:08:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,31 @@ const axios = require('axios');
 const PORT = 3000;
 
 const app = express();
-const server = http.createServer(app);
-  app.set('view engine', 'ejs');
-  app.set('json spaces', 2);
-  app.use(express.static('views'));
+app.set('view engine', 'ejs');
+app.set('json spaces', 2);
+app.use(express.static('views'));
 
 app.get('/', async (req, res) => {
     try {
         const response = await axios.get('https://web-app-bot-489f88bd8283.herokuapp.com/get');
         const data = response.data.bot;
         res.render('index', {
-           chatpc: data.chatpc,
-           user: data.user,
-           hit: data.hit,
-           uptime: data.uptime
-      });
-    };
-  });
+            chatpc: data.chatpc,
+            user: data.user,
+            hit: data.hit,
+            uptime: data.uptime
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 app.get('/tes', async (req, res) => {
     try {
-        // Lakukan permintaan ke api.xnd.me
         const response = await axios.get('https://web-app-bot-489f88bd8283.herokuapp.com/stats');
         const data = response.data.os;
 
-        // Format ulang data sesuai kebutuhan
         const formattedData = {
             CPUs: data.CPUs,
             model: data.model,
@@ -37,7 +37,6 @@ app.get('/tes', async (req, res) => {
             speed: data.speed
         };
 
-        // Kirim data ke www.xinzuo.xyz
         res.json(formattedData);
     } catch (error) {
         console.error('Error:', error);
