05175cc4cd03967c48970c64b92be049b11f7b78
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-03-01 19:46:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.set('view engine', 'ejs');
