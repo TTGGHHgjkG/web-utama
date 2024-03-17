30cb292a16ec1ea90edb54fa572fa543d24610e8
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-03-17 17:36:41 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  devOptions: {
-    port: process.env.PORT || 3000,
-  },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // Tentukan port yang akan digunakan
+  // Jika port tidak disediakan oleh lingkungan hosting, default ke port 3000
+  http: {
+    port: process.env.PORT || 3000
+  }
 });
